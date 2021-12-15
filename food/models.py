0816b24daf4e236c44fb2891f7a242c6845a134f
MarkReddy87 <markreddy87@gmail.com>
2021-12-15 15:10:18 +0000

    create mock booking model and add to admin site

--- food/models.py
+++ food/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Booking (models.Model):
+    party_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
+    booking_date = models.DateField(blank=False, null=False)
+    booking_time = models.TimeField(blank=False, null=False)
+    num_of_guests = models.IntegerField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-created_on']
