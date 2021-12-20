3503003fc93d6d6e03c63f21c301f2601fd0d64d
MarkReddy87 <markreddy87@gmail.com>
2021-12-20 16:00:11 +0000

    create first skeleton view

--- food/views.py
+++ food/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Contact
 
-# Create your views here.
+class ContactList(generic.ListView):
+    model = Contact
+    queryset = Contact.objects.order_by('created_on')
+    template_name = 'contact.html'
+    paginate_by = 5
+
