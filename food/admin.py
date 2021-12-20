71a7f63763a9dfb6428245fa8a7e823a5233b04c
MarkReddy87 <markreddy87@gmail.com>
2021-12-20 12:53:46 +0000

    add contact model and update booking model. Edit admin.py

--- food/admin.py
+++ food/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Booking, Contact
 
-admin.site.register(Booking)
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('party_name', 'booking_date', 'approved', 'created_on')
+    search_fields = ['party_name', 'phone', 'booking_date']
+    list_filter = ('approved', 'created_on')
+    actioins = ['approved_bookings']
+
+    def approved_bookings(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'name', 'created_on')
+    search_fields = ['name', 'email', 'title']
+    list_filter = ('email', 'name')
--- food/models.py
+++ food/models.py
@@ -1,14 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Booking (models.Model):
-    party_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking")
+    party_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_name")
     booking_date = models.DateField(blank=False, null=False)
     booking_time = models.TimeField(blank=False, null=False)
     num_of_guests = models.IntegerField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_on']
+
+    def __str__(self):
+        return self.party_name
+
+
+class Contact (models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment_by")
+    email = models.EmailField()
+    body = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
