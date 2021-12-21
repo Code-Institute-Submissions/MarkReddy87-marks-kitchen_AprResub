89f614658506b616691b6b54b72054f0c1f7c685
MarkReddy87 <markreddy87@gmail.com>
2021-12-21 12:22:40 +0000

    wire up home page urls and test css is linked

--- food/views.py
+++ food/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.views import generic
+from django.template import loader
+from django.http import HttpResponse
+from django.views import generic, View
 from .models import Contact
 
-class ContactList(generic.ListView):
-    model = Contact
-    queryset = Contact.objects.order_by('created_on')
-    template_name = 'contact.html'
-    paginate_by = 5
+
+class ShowMenu(generic.ListView):
+    template_name = 'index.html'
+    queryset = HttpResponse
+
+
+# class ContactList(generic.ListView):
+#     model = Contact
+#     queryset = Contact.objects.order_by('created_on')
+#     template_name = 'contact.html'
+#     paginate_by = 5
+
 
