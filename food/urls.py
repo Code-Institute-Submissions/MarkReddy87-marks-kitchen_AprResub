38c4f5d7c9cf12cf980c4056e4ac924cce4ebec6
MarkReddy87 <markreddy87@gmail.com>
2021-12-22 17:06:07 +0000

    edit login, logout and signup html pages

--- food/urls.py
+++ food/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+from django.conf.urls import url
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path('', views.ShowMenu.as_view(), name='menu')
+    path('', views.ShowMenu.as_view(), name='menu'),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
